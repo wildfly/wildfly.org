cf8926c3fdde4b620e233d5b322e25c0fb51b2b4
rayploski <ray.ploski@gmail.com>
2013-04-18 08:16:21 -0300

    Awestruct init and splashpage copy draft.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,8 +1,23 @@
 require File.join File.dirname(__FILE__), 'tweakruby'
 require_relative 'common'
+require 'awestruct/extensions/remotePartial'
+require_relative 'remotePartial'
+
+
+
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new( '/news' ) 
+
   extension Awestruct::Extensions::Indexifier.new
+
+  transformer Awestruct::Extensions::Minify.new
+
+  helper Awestruct::Extensions::RemotePartial
+  helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::GoogleAnalytics
+
+  # Generate a sitemap.xml
+  extension Awestruct::Extensions::Sitemap.new
+
 end
 
