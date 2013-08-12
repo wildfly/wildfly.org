2f075dbcc3cfacfa7685c11fb15efcd52b71f38d
Jason T. Greene <jason.greene@redhat.com>
2013-08-11 20:08:24 -0500

    Generate download table using simplified config and http for sizes

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,11 +4,11 @@ require_relative 'common'
 require 'awestruct/extensions/remotePartial'
 require_relative 'remotePartial'
 require 'awestruct/extensions/minify'
-
+require_relative 'releases'
 Awestruct::Extensions::Pipeline.new do
 
   extension Awestruct::Extensions::Indexifier.new
-
+  extension Release.new
   helper Awestruct::Extensions::RemotePartial
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::GoogleAnalytics
