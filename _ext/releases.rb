f5f03616803df25b871e8550ff87f6a36c86238a
Jason T. Greene <jason.greene@redhat.com>
2015-07-23 23:23:00 -0500

    Release 9.0.1 and 8.2.1

--- _ext/releases.rb
+++ _ext/releases.rb
@@ -27,14 +27,14 @@ class Release
           release[:servlet][kind] = {:url => uri, :size => size}
         end
       end
+      if release.has_key?("updateforversion")
+        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}-update.zip")
+        release[:update] = {:url => uri, :size => compute_size(uri)}
+      end
       if release.has_key?("quickversion")
         version = release[:quickversion] 
         uri = URI.parse("http://download.jboss.org/wildfly/#{version}/quickstart-#{version}.zip")
         release[:quickstart] = {:url => uri, :size => compute_size(uri)}
-      end
-      if release.has_key?("updateforversion")
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}-update.zip")
-        release[:update] = {:url => uri, :size => compute_size(uri)}
       end
     end
   end
