bc93b54a4b648c669a6a656a75e6e5cf2f864e99
Jason T. Greene <jason.greene@redhat.com>
2015-10-27 00:42:54 -0500

    Release 9.0.2 & reuse HTTP connection

--- _ext/releases.rb
+++ _ext/releases.rb
@@ -7,47 +7,53 @@ class Release
   end
 
   def execute(site)
-    (site.releases).each do |release|
-      version = release[:version]
-      {:zip => '.zip',  :tgz => '.tar.gz', :srczip => '-src.zip', :srctgz => '-src.tar.gz'}.each do |kind, suffix|
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}#{suffix}")
-        release[kind] = {:url => uri, :size => compute_size(uri)}
-      end
-      {:zip => '.zip',  :tgz => '.tar.gz', :srczip => '-src.zip', :srctgz => '-src.tar.gz'}.each do |kind, suffix|
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/servlet/wildfly-web-#{version}#{suffix}")
-        size = compute_size(uri)
-        if (size != "unknown")
-          release[:servlet] = {} unless release.has_key?(:servlet)
-          release[:servlet][kind] = {:url => uri, :size => size}
+    Net::HTTP.start("download.jboss.org") do |http|
+      (site.releases).each do |release|
+        version = release[:version]
+        {:zip => '.zip',  :tgz => '.tar.gz', :srczip => '-src.zip', :srctgz => '-src.tar.gz'}.each do |kind, suffix|
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}#{suffix}")
+          release[kind] = {:url => uri, :size => compute_size(http,uri)}
         end
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/servlet/wildfly-servlet-#{version}#{suffix}")
-        size = compute_size(uri)
-        if (size != "unknown")
-          release[:servlet] = {} unless release.has_key?(:servlet)
-          release[:servlet][kind] = {:url => uri, :size => size}
+        {:zip => '.zip',  :tgz => '.tar.gz', :srczip => '-src.zip', :srctgz => '-src.tar.gz'}.each do |kind, suffix|
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/servlet/wildfly-web-#{version}#{suffix}")
+          size = compute_size(http,uri)
+          if (size != "unknown")
+            release[:servlet] = {} unless release.has_key?(:servlet)
+            release[:servlet][kind] = {:url => uri, :size => size}
+          end
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/servlet/wildfly-servlet-#{version}#{suffix}")
+          size = compute_size(http,uri)
+          if (size != "unknown")
+            release[:servlet] = {} unless release.has_key?(:servlet)
+            release[:servlet][kind] = {:url => uri, :size => size}
+          end
         end
-      end
-      if release.has_key?("updateforversion")
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}-update.zip")
-        release[:update] = {:url => uri, :size => compute_size(uri)}
-      end
-      if release.has_key?("quickversion")
-        version = release[:quickversion] 
-        uri = URI.parse("http://download.jboss.org/wildfly/#{version}/quickstart-#{version}.zip")
-        release[:quickstart] = {:url => uri, :size => compute_size(uri)}
+        if release.has_key?("updateforversion")
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}-update.zip")
+          release[:update] = {:url => uri, :size => compute_size(http,uri)}
+        end
+        if release.has_key?("updateforversionfull")
+          versionfull = release["updateforversionfull"]
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/wildfly-#{version}-#{versionfull}-update.zip")
+          release[:updatefull] = {:url => uri, :size => compute_size(http,uri)}
+        end
+        if release.has_key?("quickversion")
+          version = release[:quickversion] 
+          uri = URI.parse("http://download.jboss.org/wildfly/#{version}/quickstart-#{version}.zip")
+          release[:quickstart] = {:url => uri, :size => compute_size(http,uri)}
+        end
       end
     end
   end
 
-  def compute_size(uri)
-    Net::HTTP.start( uri.host, uri.port ) do |http|
-      response = http.head( uri.path )
-      b = response['content-length'] || ''
-      if ( response.code == "200" and ! b.empty? )
-         formatBytes(b)
-      else
-        'unknown'
-      end
+  def compute_size(http, uri)
+    response = http.head( uri.path )
+    b = response['content-length'] || ''
+    puts "#{uri.path} #{response.code}"
+    if ( response.code == "200" and ! b.empty? )
+       formatBytes(b)
+    else
+      'unknown'
     end
   end
 
