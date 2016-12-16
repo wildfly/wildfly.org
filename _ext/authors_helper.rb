bd30a29d247bb00d8cfc1828b73c3ea66e89c445
Jason T. Greene <jason.greene@redhat.com>
2016-12-16 14:34:17 -0600

    Add support for twitter summary cards

--- _ext/authors_helper.rb
+++ _ext/authors_helper.rb
@@ -1,6 +1,6 @@
 module Awestruct::Extensions::AuthorsHelper
   def lookup_name(nick)
-    author = site.authors[nick]
+    author = nick ? site.authors[nick] : nil
     name = author ? author["name"] : nick
     name ? name : "Unknown"
   end
