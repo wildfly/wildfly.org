16571e9eee1741800c92fbd0fe6095fe4e7aaa7a
James Cobb <jcobb@redhat.com>
2024-05-07 16:18:19 -0500

    Matomo POC closing, remove code

--- assets/javascript/tracking.js
+++
@@ -1,8 +0,0 @@
-var idSite = 5;
-var matomoTrackingApiUrl = 'https://analytics.ossupstream.org/matomo.php';
-
-var _paq = window._paq = window._paq || [];  
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);  
