d457b6a09bd040a7880f28e644cb3745456124cc
Jason Lee <jason@steeplesoft.com>
2025-08-21 09:50:31 -0500

    Update build to Roq 1.8.1
    Remove adoc escapes since Roq now handles this more cleanly
    Use standard adoc includes instead of Qute includes
    Set global imagesdir property to fix broken images

--- src/main/java/org/wildfly/site/Extensions.java
+++ src/main/java/org/wildfly/site/Extensions.java
@@ -1,15 +1,13 @@
 package org.wildfly.site;
 
+import io.quarkiverse.qute.web.markdown.runtime.MdConverter;
+import io.quarkus.arc.Arc;
+import io.quarkus.qute.TemplateExtension;
+
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
-import io.quarkiverse.qute.web.markdown.runtime.MdConverter;
-import io.quarkiverse.roq.frontmatter.runtime.model.DocumentPage;
-import io.quarkiverse.roq.plugin.asciidoctorj.runtime.AsciidoctorJConverter;
-import io.quarkus.arc.Arc;
-import io.quarkus.qute.TemplateExtension;
 
 @TemplateExtension
 public class Extensions {
@@ -28,12 +26,6 @@ public class Extensions {
 
     public static String join(List<?> list, String separator) {
         return String.join(separator, list.stream().map(Object::toString).toList());
-    }
-
-    public static String asciidoc(String rawText) {
-        AsciidoctorJConverter converter = Arc.container().beanInstanceSupplier(AsciidoctorJConverter.class).get().get();
-
-        return converter.apply(rawText.replaceAll("\\{[#/]asciidoc}", ""));
     }
 
     public static String markdown(String rawText) {
